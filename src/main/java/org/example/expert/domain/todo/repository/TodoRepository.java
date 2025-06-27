f2e3452f5d0bda028d94a9370500d7ddf41c1ca2
kimwonjun1 <monkeydmw@naver.com>
2025-06-27 19:35:02 +0900

    lv 1-3. 코드 개선 퀴즈 -  JPA의 이해 - TodoController getTodos weather, startDate, endDate RequestParam 추가, TodoRepository searchTodoWithUser Query 작성, TodoService getTodos searchTodoWitchUser 사용

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,8 +1,10 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDateTime;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +20,17 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT t FROM Todo t " +
+        "WHERE (:weather IS NULL OR t.weather = :weather) " +
+        "AND (:start IS NULL OR t.modifiedAt >= :start) " +
+        "AND (:end IS NULL OR t.modifiedAt <= :end)" +
+        "ORDER BY t.modifiedAt DESC")
+    Page<Todo> searchTodosWithUser(
+        @Param("weather") String weather,
+        @Param("start") LocalDateTime start,
+        @Param("end") LocalDateTime end,
+        Pageable pageable
+    );
 }
